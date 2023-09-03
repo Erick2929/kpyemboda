e31137d2d8c16ed0a986b99a143f8dcd0bfd3085
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-09-02 20:48:37 -0600

    Initial commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <div>Footer hola 2</div>;
+}
+
+export default Footer;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <div>Navbar Hola</div>;
+}
+
+export default Navbar;
