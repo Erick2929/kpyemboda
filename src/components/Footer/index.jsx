f5f5c01ab36934bed9c807dec2f27cc73a4bac27
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-09-07 08:36:05 -0600

    Desi Logo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,45 +1,47 @@
 import { IconButton } from "@mui/material";
 import styles from "./styles.module.css";
 import { Instagram, WhatsApp } from "@mui/icons-material";
+import desi from "../../assets/imgs/desi.png";
 
 function Footer() {
   const mensaje =
     "Hola, estoy interesado en cotizar un desarrollo. ¿Podrías ayudarme?";
   return (
-    <div className={styles.containerFather}>
-      <div className={styles.container} id='footer'>
-        <div className={styles.iconAtag}>
-          <IconButton
-            href={"https://wa.me/8442895076?text=" + mensaje}
-            target='_blank'
-          >
-            <WhatsApp />
-          </IconButton>
-          <a
-            href={"https://wa.me/8442895076?text=" + mensaje}
-            target='_blank'
-            rel='noreferrer'
-          >
-            {" "}
-            8442895076
-          </a>
-        </div>
-        <div className={styles.iconAtag}>
-          <IconButton
-            href='https://www.instagram.com/desisoftware/'
-            target='_blank'
-          >
-            <Instagram />
-          </IconButton>
-          <a
-            href='https://www.instagram.com/desisoftware/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            @desisoftware
-          </a>
-        </div>
+    <div className={styles.container} id='footer'>
+      <div className={styles.iconAtag}>
+        <IconButton
+          href={"https://wa.me/8442895076?text=" + mensaje}
+          target='_blank'
+        >
+          <WhatsApp />
+        </IconButton>
+        <a
+          href={"https://wa.me/8442895076?text=" + mensaje}
+          target='_blank'
+          rel='noreferrer'
+        >
+          {" "}
+          8442895076
+        </a>
       </div>
+      <div className={styles.iconAtag}>
+        <IconButton
+          href='https://www.instagram.com/desisoftware/'
+          target='_blank'
+        >
+          <Instagram />
+        </IconButton>
+        <a
+          href='https://www.instagram.com/desisoftware/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          @desisoftware
+        </a>
+      </div>
+      <a href={"https://wa.me/8442895076?text=" + mensaje}>
+        <img src={desi} alt='desi-logo' className={styles.logo} />
+      </a>
     </div>
   );
 }
